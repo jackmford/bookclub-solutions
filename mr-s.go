520aa363e450c572d4df11be5ac05696c2872c35
jackmford <jackmitchellfordyce@gmail.com>
2022-03-09 11:33:09 -0600

    final case

--- mr-s.go
+++ mr-s.go
@@ -23,19 +23,17 @@ func main() {
     fileScanner.Scan()
     total_hrs := 0
     total_mins := 0
-    days := 0 
+    days := 0
+    var total_time float64
 
     for fileScanner.Scan() {
         days = days+1
         line := fileScanner.Text()
-        fmt.Println(line)
         am_hour := strings.Split(line, " ")[0]
         am_hour_int, _ := strconv.Atoi(strings.Split(am_hour, ":")[0])
 
         am_or_pm_to_bed := strings.Split(line, " ")[1]
         am_or_pm_to_rise := strings.Split(line, " ")[3]
-        fmt.Println(am_or_pm_to_bed)
-        fmt.Println(am_or_pm_to_rise)
 
         pm_hour := strings.Split(line, " ")[2]
         pm_hour_int, _ := strconv.Atoi(strings.Split(pm_hour, ":")[0])
@@ -64,19 +62,10 @@ func main() {
         }
 
         if am_or_pm_to_bed == "AM," && am_or_pm_to_rise == "PM" {
-            total_hrs = total_hrs+(pm_hour_int-am_hour_int)
-            total_hrs = total_hrs+(12-am_hour_int+pm_hour_int)
+            total_hrs = total_hrs+((12-am_hour_int)+pm_hour_int)
         }
-
-        fmt.Println(am_hour_int)
-        fmt.Println(pm_hour_int)
-        fmt.Println(total_hrs)
-        fmt.Println(total_mins)
-        var total_time float64
         total_time = float64((total_hrs*60+total_mins))/float64(60)
         total_time = total_time/float64(days)
-        fmt.Println(total_time)
-        fmt.Printf("%.1f hours", total_time)
-
     }
+    fmt.Printf("%.1f hours", total_time)
 }
