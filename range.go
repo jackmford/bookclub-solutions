19c33f58b5b6537dc992b6def4f8a438169303c1
jackmford <jackmitchellfordyce@gmail.com>
2022-04-06 11:56:44 -0500

    hope to discuss this, a bit confused

--- range.go
+++ range.go
@@ -4,11 +4,22 @@ import (
   "fmt"
 )
 
+func sendData(ch chan int, value int) {
+  for i:=0; i<value; i++ {
+    ch <- i
+  }
+  close(ch)
+}
+
 func xrange(n int) chan int {
   // IMPLEMENT
-  return nil
+  ch := make(chan int)
+  go sendData(ch, n)
+  return ch
 }
 
 func main() {
-  fmt.Println("hi")
+  for i := range xrange(5) {
+    fmt.Println(i)
+  }
 }
